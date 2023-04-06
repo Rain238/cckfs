84e3ad9cfa7c53b1a4b3be43ec60b03762f1ecb8
Rain238 <3164395730@qq.com>
2023-04-06 16:14:28 +0800

    启动类

---
+++ src/main/java/com/rain/cckfs/CckfsApplication.java
@@ -0,0 +1,15 @@
+package com.rain.cckfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @author LightRain
+ */
+@SpringBootApplication
+public class CckfsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CckfsApplication.class, args);
+    }
+
+}
