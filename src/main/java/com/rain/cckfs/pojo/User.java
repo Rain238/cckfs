2fbb78d530c119e323136d7c9fd2f5e02c781552
Rain238 <3164395730@qq.com>
2023-04-06 16:09:39 +0800

    User

---
+++ src/main/java/com/rain/cckfs/pojo/User.java
@@ -0,0 +1,66 @@
+package com.rain.cckfs.pojo;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.List;
+
+/**
+ * @Author: LightRain
+ * @Description: User
+ * @DateTime: 2023-04-01 16:37
+ * @Version：1.0
+ **/
+@Setter
+@Getter
+@ToString
+@Data
+public class User implements UserDetails {   //实现UserDeails接口
+    /**
+     * id
+     */
+    private int id;
+    /**
+     * 用户名
+     */
+    private String username;
+    /**
+     * 密码
+     */
+    private String password;
+    /**
+     * 验证码
+     */
+    private String captcha;
+    private List<GrantedAuthority> authorities;
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
+
