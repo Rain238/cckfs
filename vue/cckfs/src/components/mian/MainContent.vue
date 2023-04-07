3ee3624f638aca8f6e04331ac352847425e96e66
Rain238 <3164395730@qq.com>
2023-04-07 22:13:13 +0800

    添加session状态检测

--- vue/cckfs/src/components/mian/MainContent.vue
+++ vue/cckfs/src/components/mian/MainContent.vue
@@ -8,8 +8,33 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: "MainContent"
+  name: "MainContent",
+  methods: {
+    /**
+     * session状态
+     */
+    sessionStatus() {
+      axios({
+        url: "/api/session/status",
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      }).then(res => {
+        console.log(res)
+        if (res.data.status === "FAILED") {
+          window.location.href='/login';
+          ElMessage.error(res.data.message)
+        }
+      })
+    },
+  },
+  mounted() {
+    this.sessionStatus()
+  }
 }
 </script>
 
